e0a4e14efebc0a907f9b76a2cd94b3eff13818df
saranyatinku <bsaranya303@gmail.com>
2026-08-11 12:19:23 +0530

    Completed question 5

---
+++ question5.py
@@ -0,0 +1,8 @@
+message = input("Enter message: ")
+
+print("First 5 Characters:", message[:5])
+print("Last 5 Characters:", message[-5:])
+print("Characters from Index 2 to 7:", message[2:8])
+print("Every Second Character:", message[::2])
+print("Message in Reverse:", message[::-1])
+print("Message Without First and Last Char:", message[1:-1])
